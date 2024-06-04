dbbf4ec2ec369a62f281a132c78ef0207829ea3e
Klebson Mateus <klebsonbm@gmail.com>
2024-06-04 19:58:50 -0300

    Criar lista de times

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,21 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 const Formulario = () => {
+
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+
+
     return (
         <section className='formulario'>
             <form>
@@ -8,6 +23,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
